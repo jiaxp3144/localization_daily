a2aa9d9c83ee3fcac5aa098e834c3bff58cdcf27
jiaxp3144 <jxp12@mails.tsinghua.edu.cn>
2017-06-06 17:38:18 +0800

    tested GCC

--- localization/tdoalib.py
+++ localization/tdoalib.py
@@ -4,11 +4,13 @@
 import numpy as np
 from scipy import signal
 from scipy import ifft
+from scipy.stats import mode
 
 # test only
 import wavlib
+import matplotlib.pyplot as plt
 
-def GCC(wave_data, params, weight_fun=None, frame_len=512):
+def GCC(wave_data, params, weight_fun=None, frame_len=512, ref_power=None):
   x1 = wave_data[:,0]
   x2 = wave_data[:,1]
   freq = params[2]
@@ -18,9 +20,12 @@ def GCC(wave_data, params, weight_fun=None, frame_len=512):
   f,t,X1 = signal.stft(x1, freq, window=win, nperseg=frame_len)
   f,t,X2 = signal.stft(x2, freq, window=win, nperseg=frame_len)
   [a, frame_num] = X1.shape
+  
   # calc Gx1x2
   X = X1*(X2.conjugate())
-  if weight_fun.lower() == 'scot':
+  if weight_fun==None:
+    Y = 1
+  elif weight_fun.lower() == 'scot':
     X11 = X1 * (X1.conjugate())
     X22 = X2 * (X2.conjugate())
     Y = np.sqrt(X11*X22)
@@ -28,20 +33,36 @@ def GCC(wave_data, params, weight_fun=None, frame_len=512):
     Y = np.abs(X1 * (X1.conjugate()))
   elif weight_fun.lower() == 'phat':
     Y = np.abs(X)
-  elif weight_fun==None:
-    Y = 1
   else:
     print('ERROR: unrecognised weight function for GCC!')
     return
-  Gxx = ifft(X/Y,axis=0).real()
+  Gxx = ifft(X/(Y+1e-15),axis=0).real
   
+  # calc ref_power
+  if not ref_power:
+    X11 = X1 * (X1.conjugate())
+    ref_power = np.sum(X11)/(frame_num*5.0)
+  
+  time_delay = []
+  for i in range(frame_num):
+    if np.sum(X11[:,i])<ref_power:
+      continue
+    time_delay.append(np.argmax(Gxx[:,i]))
+    
+  # use the most frequently appeared value as the estimated value
+  delay = mode(time_delay).mode[0]
+  # if r is after l, the delay should be minus
+  if delay > 128:
+    delay = delay-257
+  return delay
+    
   
   
   
 
 if __name__=='__main__':
-  filename = 'test_d.wav'
-  wave_data, params = wavlib.audioRead(filename)
-  print(wave_data.shape)
-  print(params)
-  GCC(wave_data, params)
+  for azim in range(0,361,5):
+    filename = 'test_{0}.wav'.format(azim)
+    wave_data, params = wavlib.audioRead(filename)
+    time_delay = GCC(wave_data, params,weight_fun=None)
+    print("azim={0},delay point={1}".format(azim,time_delay))
--- localization/testDatalib.py
+++ localization/testDatalib.py
@@ -9,22 +9,23 @@ import wavlib
 
 if __name__=='__main__':
   # test settings
-  azim = 120
-  elev = 0
-  dist = 160
-  hrir_path = r'..\..\resources\hrir_txt'
+  for azim in range(0,361,5):
+    elev = 0
+    dist = 160
+    hrir_path = r'..\..\resources\hrir_txt'
+    '''
+    # test readHrir()
+    (hrir_r, hrir_l) = datalib.readHrir(hrir_path, azim, elev, dist)
+    plt.subplot(211)
+    plt.plot(hrir_r)
+    plt.subplot(212)
+    plt.plot(hrir_l)
+    plt.show()
+    '''
+    # test addDirection
+    (wave_data, params) = wavlib.audioRead('test.wav')
+    (wave_data_new, params_new) = datalib.addDirection(\
+      wave_data, params, azim, elev, dist, hrir_path)
+    #wavlib.audioPlay(wave_data_new,params_new)
+    wavlib.audioWrite('test_{0}.wav'.format(azim),params_new,wave_data_new)
   
-  # test readHrir()
-  (hrir_r, hrir_l) = datalib.readHrir(hrir_path, azim, elev, dist)
-  plt.subplot(211)
-  plt.plot(hrir_r)
-  plt.subplot(212)
-  plt.plot(hrir_l)
-  plt.show()
-  
-  # test addDirection
-  (wave_data, params) = wavlib.audioRead('test.wav')
-  (wave_data_new, params_new) = datalib.addDirection(\
-    wave_data, params, azim, elev, dist, hrir_path)
-  wavlib.audioPlay(wave_data_new,params_new)
-  wavlib.audioWrite('test_d.wav',params_new,wave_data_new)
